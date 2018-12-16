d6972b3c5c33ab0f0f46e01112de05312ed02d1d
Leo Maden <leo.c.maden@gmail.com>
2018-12-16 23:08:41 +0000

    Added basic log in that authenticates hard coded values - needs to validate from database.

---
+++ AirlineBookingLibrary/Helpers/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineBookingLibrary.Helpers
+{
+    public static class StringExtensions
+    {
+        public static string NullIfEmpty(this string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return null;
+            }
+            else
+            {
+                return s;
+            }
+        }
+    }
+}
---
+++ WebUI/Models/LogInModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Models
+{
+    public class LogInModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+
+        [HiddenInput]
+        public string ReturnUrl { get; set; }
+
+    }
+}
