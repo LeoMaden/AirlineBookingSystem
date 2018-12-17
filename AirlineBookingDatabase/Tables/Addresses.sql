f522606bd68d4088053ad48c41b23a1cadd36c3e
Leo Maden <leo.c.maden@gmail.com>
2018-12-17 21:59:03 +0000

    Created tables in database that map to library classes.

---
+++ AirlineBookingDatabase/Tables/Addresses.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Addresses]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [StreetAddress] NVARCHAR(100) NOT NULL, 
+    [Locality] NVARCHAR(100) NOT NULL, 
+    [City] NVARCHAR(100) NOT NULL, 
+    [Postcode] NVARCHAR(10) NOT NULL, 
+    CONSTRAINT [FK_Addresses_Users] FOREIGN KEY (Id) REFERENCES Users(Id)
+)
---
+++ AirlineBookingDatabase/Tables/Users.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] NVARCHAR(10) NOT NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [UserName] NVARCHAR(100) NOT NULL, 
+    [DateOfBirth] DATE NOT NULL, 
+    [Email] NVARCHAR(150) NOT NULL, 
+    [PhoneNumber] NVARCHAR(11) NOT NULL, 
+    [DateCreated] DATE NOT NULL, 
+    [PasswordHash] NVARCHAR(MAX) NOT NULL
+)
