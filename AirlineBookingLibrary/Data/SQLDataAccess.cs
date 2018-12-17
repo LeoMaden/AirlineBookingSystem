362beb727035dac158d5285114f0ab2327cece9f
Leo Maden <leo.c.maden@gmail.com>
2018-12-17 21:10:20 +0000

    Moved database access logic to class library, making UserStore dependant on an IDataAccess from GlobalConfig.

--- AirlineBookingLibrary/Data/SQLDataAccess.cs
+++ AirlineBookingLibrary/Data/SQLDataAccess.cs
@@ -3,10 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AirlineBookingLibrary.Models;
 
 namespace AirlineBookingLibrary.Data
 {
-    public class SQLDataAccess //: IDataAccess
+    public class SQLDataAccess : IDataAccess
     {
+        public async Task CreateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> FindByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> FindByIdAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> FindByNameAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetEmailAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetPasswordHashAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetPhoneNumberAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> HasPasswordAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SetEmailAsync(User user, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SetPasswordHashAsync(User user, string passwordHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SetPhoneNumberAsync(User user, string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- WebUI/Models/IdentityModels/UserStore.cs
+++ WebUI/Models/IdentityModels/UserStore.cs
@@ -1,4 +1,5 @@
-﻿using AirlineBookingLibrary.Models;
+﻿using AirlineBookingLibrary;
+using AirlineBookingLibrary.Models;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -11,19 +12,19 @@ namespace WebUI.Models.IdentityModels
 {
     public class UserStore : IUserStore<ApplicationUser, int>, IUserClaimStore<ApplicationUser, int>, IUserEmailStore<ApplicationUser, int>, IUserPasswordStore<ApplicationUser, int>, IUserPhoneNumberStore<ApplicationUser, int>
     {
-        Task IUserClaimStore<ApplicationUser, int>.AddClaimAsync(ApplicationUser user, Claim claim)
+        async Task IUserClaimStore<ApplicationUser, int>.AddClaimAsync(ApplicationUser user, Claim claim)
         {
             throw new NotImplementedException();
         }
 
-        Task IUserStore<ApplicationUser, int>.CreateAsync(ApplicationUser user)
+        async Task IUserStore<ApplicationUser, int>.CreateAsync(ApplicationUser user)
         {
-            throw new NotImplementedException();
+            await GlobalConfig.DbContext.CreateAsync(user);
         }
 
-        Task IUserStore<ApplicationUser, int>.DeleteAsync(ApplicationUser user)
+        async Task IUserStore<ApplicationUser, int>.DeleteAsync(ApplicationUser user)
         {
-            throw new NotImplementedException();
+            await GlobalConfig.DbContext.DeleteAsync(user);
         }
 
         void IDisposable.Dispose()
@@ -31,89 +32,89 @@ namespace WebUI.Models.IdentityModels
             throw new NotImplementedException();
         }
 
-        Task<ApplicationUser> IUserEmailStore<ApplicationUser, int>.FindByEmailAsync(string email)
+        async Task<ApplicationUser> IUserEmailStore<ApplicationUser, int>.FindByEmailAsync(string email)
+        {
+            return await GlobalConfig.DbContext.FindByEmailAsync(email) as ApplicationUser;
+        }
+
+        async Task<ApplicationUser> IUserStore<ApplicationUser, int>.FindByIdAsync(int userId)
+        {
+            return await GlobalConfig.DbContext.FindByIdAsync(userId) as ApplicationUser;
+        }
+
+        async Task<ApplicationUser> IUserStore<ApplicationUser, int>.FindByNameAsync(string userName)
+        {
+            return await GlobalConfig.DbContext.FindByNameAsync(userName) as ApplicationUser;
+        }
+
+        async Task<IList<Claim>> IUserClaimStore<ApplicationUser, int>.GetClaimsAsync(ApplicationUser user)
         {
             throw new NotImplementedException();
         }
 
-        Task<ApplicationUser> IUserStore<ApplicationUser, int>.FindByIdAsync(int userId)
+        async Task<string> IUserEmailStore<ApplicationUser, int>.GetEmailAsync(ApplicationUser user)
+        {
+            return await GlobalConfig.DbContext.GetEmailAsync(user);
+        }
+
+        async Task<bool> IUserEmailStore<ApplicationUser, int>.GetEmailConfirmedAsync(ApplicationUser user)
         {
             throw new NotImplementedException();
         }
 
-        Task<ApplicationUser> IUserStore<ApplicationUser, int>.FindByNameAsync(string userName)
+        async Task<string> IUserPasswordStore<ApplicationUser, int>.GetPasswordHashAsync(ApplicationUser user)
+        {
+            return await GlobalConfig.DbContext.GetPasswordHashAsync(user);
+        }
+
+        async Task<string> IUserPhoneNumberStore<ApplicationUser, int>.GetPhoneNumberAsync(ApplicationUser user)
+        {
+            return await GlobalConfig.DbContext.GetPhoneNumberAsync(user);
+        }
+
+        async Task<bool> IUserPhoneNumberStore<ApplicationUser, int>.GetPhoneNumberConfirmedAsync(ApplicationUser user)
         {
             throw new NotImplementedException();
         }
 
-        Task<IList<Claim>> IUserClaimStore<ApplicationUser, int>.GetClaimsAsync(ApplicationUser user)
+        async Task<bool> IUserPasswordStore<ApplicationUser, int>.HasPasswordAsync(ApplicationUser user)
+        {
+            return await GlobalConfig.DbContext.HasPasswordAsync(user);
+        }
+
+        async Task IUserClaimStore<ApplicationUser, int>.RemoveClaimAsync(ApplicationUser user, Claim claim)
         {
             throw new NotImplementedException();
         }
 
-        Task<string> IUserEmailStore<ApplicationUser, int>.GetEmailAsync(ApplicationUser user)
+        async Task IUserEmailStore<ApplicationUser, int>.SetEmailAsync(ApplicationUser user, string email)
+        {
+            await GlobalConfig.DbContext.SetEmailAsync(user, email);
+        }
+
+        async Task IUserEmailStore<ApplicationUser, int>.SetEmailConfirmedAsync(ApplicationUser user, bool confirmed)
         {
             throw new NotImplementedException();
         }
 
-        Task<bool> IUserEmailStore<ApplicationUser, int>.GetEmailConfirmedAsync(ApplicationUser user)
+        async Task IUserPasswordStore<ApplicationUser, int>.SetPasswordHashAsync(ApplicationUser user, string passwordHash)
+        {
+            await GlobalConfig.DbContext.SetPasswordHashAsync(user, passwordHash);
+        }
+
+        async Task IUserPhoneNumberStore<ApplicationUser, int>.SetPhoneNumberAsync(ApplicationUser user, string phoneNumber)
+        {
+            await GlobalConfig.DbContext.SetPhoneNumberAsync(user, phoneNumber);
+        }
+
+        async Task IUserPhoneNumberStore<ApplicationUser, int>.SetPhoneNumberConfirmedAsync(ApplicationUser user, bool confirmed)
         {
             throw new NotImplementedException();
         }
 
-        Task<string> IUserPasswordStore<ApplicationUser, int>.GetPasswordHashAsync(ApplicationUser user)
+        async Task IUserStore<ApplicationUser, int>.UpdateAsync(ApplicationUser user)
         {
-            throw new NotImplementedException();
-        }
-
-        Task<string> IUserPhoneNumberStore<ApplicationUser, int>.GetPhoneNumberAsync(ApplicationUser user)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<bool> IUserPhoneNumberStore<ApplicationUser, int>.GetPhoneNumberConfirmedAsync(ApplicationUser user)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<bool> IUserPasswordStore<ApplicationUser, int>.HasPasswordAsync(ApplicationUser user)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task IUserClaimStore<ApplicationUser, int>.RemoveClaimAsync(ApplicationUser user, Claim claim)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task IUserEmailStore<ApplicationUser, int>.SetEmailAsync(ApplicationUser user, string email)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task IUserEmailStore<ApplicationUser, int>.SetEmailConfirmedAsync(ApplicationUser user, bool confirmed)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task IUserPasswordStore<ApplicationUser, int>.SetPasswordHashAsync(ApplicationUser user, string passwordHash)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task IUserPhoneNumberStore<ApplicationUser, int>.SetPhoneNumberAsync(ApplicationUser user, string phoneNumber)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task IUserPhoneNumberStore<ApplicationUser, int>.SetPhoneNumberConfirmedAsync(ApplicationUser user, bool confirmed)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task IUserStore<ApplicationUser, int>.UpdateAsync(ApplicationUser user)
-        {
-            throw new NotImplementedException();
+            await GlobalConfig.DbContext.UpdateAsync(user);
         }
     }
 }
