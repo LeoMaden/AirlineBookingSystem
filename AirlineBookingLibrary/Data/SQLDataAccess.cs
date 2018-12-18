2c2f4ca30f72bc92d80042cad6430fde6f833575
Leo Maden <leo.c.maden@gmail.com>
2018-12-18 00:06:13 +0000

    Implemented 2 methods in SQLDataAccess using stored procedures.

--- AirlineBookingLibrary/Data/SQLDataAccess.cs
+++ AirlineBookingLibrary/Data/SQLDataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,35 @@ namespace AirlineBookingLibrary.Data
 {
     public class SQLDataAccess : IDataAccess
     {
+        private IDbConnection Connection
+        {
+            get
+            {
+                return new System.Data.SqlClient.SqlConnection(GlobalConfig.ConnectionString);
+            }
+        }
+
         public async Task CreateAsync(User user)
         {
-            throw new NotImplementedException();
+            using (IDbConnection connection = Connection)
+            {
+                // Add parameters to query.
+                var p = new DynamicParameters();
+                p.Add("@Title", user.Title);
+                p.Add("@FirstName", user.FirstName);
+                p.Add("@LastName", user.LastName);
+                p.Add("@UserName", user.UserName);
+                p.Add("@DateOfBirth", user.DateOfBirth);
+                p.Add("@Email", user.Email);
+                p.Add("@PhoneNumber", user.PhoneNumber);
+                p.Add("@DateCreated", user.DateCreated);
+                p.Add("@Id", user.Id, direction: ParameterDirection.Output);
+
+                await connection.ExecuteAsync("spInsertUser", p, commandType: CommandType.StoredProcedure);
+
+                // Set user Id to value of output parameter.
+                user.Id = p.Get<int>("@Id");
+            }
         }
 
         public async Task DeleteAsync(User user)
@@ -62,7 +89,14 @@ namespace AirlineBookingLibrary.Data
 
         public async Task SetPasswordHashAsync(User user, string passwordHash)
         {
-            throw new NotImplementedException();
+            using (IDbConnection connection = Connection)
+            {
+                var p = new DynamicParameters();
+                p.Add("@Id", user.Id);
+                p.Add("@PasswordHash", passwordHash);
+
+                await connection.ExecuteAsync("spSetUserPasswordHash", p, commandType: CommandType.StoredProcedure);
+            }
         }
 
         public async Task SetPhoneNumberAsync(User user, string phoneNumber)
