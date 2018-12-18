5d760166f5a4db652ef9d528d6351e20f515805a
Leo Maden <leo.c.maden@gmail.com>
2018-12-18 21:32:36 +0000

    Updated Addresses table to allow nulls for Locality and City.

--- AirlineBookingDatabase/Tables/Addresses.sql
+++ AirlineBookingDatabase/Tables/Addresses.sql
@@ -2,8 +2,8 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [StreetAddress] NVARCHAR(100) NOT NULL, 
-    [Locality] NVARCHAR(100) NOT NULL, 
-    [City] NVARCHAR(100) NOT NULL, 
+    [Locality] NVARCHAR(100) NULL, 
+    [City] NVARCHAR(100) NULL, 
     [Postcode] NVARCHAR(10) NOT NULL, 
     CONSTRAINT [FK_Addresses_Users] FOREIGN KEY (Id) REFERENCES Users(Id)
 )
