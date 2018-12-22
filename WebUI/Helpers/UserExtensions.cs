62add261693ef11cca5b76554c42a18c93d859a5
Leo Maden <leo.c.maden@gmail.com>
2018-12-22 23:58:26 +0000

    User could not be cast to ApplicationUser so added extension method to do this. Cast updated in UserStore.

---
+++ WebUI/Helpers/UserExtensions.cs
@@ -0,0 +1,32 @@
+﻿using AirlineBookingLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebUI.Models.IdentityModels;
+
+namespace WebUI.Helpers
+{
+    public static class UserExtensions
+    {
+        public static ApplicationUser ToApplicationUser(this User user)
+        {
+            var output = new ApplicationUser
+            {
+                Id = user.Id,
+                Title = user.Title,
+                FirstName = user.FirstName,
+                LastName = user.FirstName,
+                UserName = user.UserName,
+                DateOfBirth = user.DateOfBirth,
+                Address = user.Address,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                DateCreated = user.DateCreated,
+                PasswordHash = user.PasswordHash
+            };
+
+            return output;
+        }
+    }
+}
