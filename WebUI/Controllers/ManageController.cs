e325151a9ce56b5936bc33e9841a4d0a0304c9b4
Leo Maden <leo.c.maden@gmail.com>
2018-12-24 12:43:50 +0000

    Added initial ManageController so user can view their account details.

---
+++ WebUI/Controllers/ManageController.cs
@@ -0,0 +1,25 @@
+﻿using AirlineBookingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Helpers;
+
+namespace WebUI.Controllers
+{
+    public class ManageController : Controller
+    {
+        //
+        // GET /Manage
+        async public Task<ActionResult> Index()
+        {
+            var userName = User.Identity.Name;
+
+            var user = (await GlobalConfig.DbContext.FindByNameAsync(userName)).ToApplicationUser();
+
+            return View(user);
+        }
+    }
+}
