fa8556cc9f835c419b231ab7c1d302ebf6d7a333
Leo Maden <leo.c.maden@gmail.com>
2018-12-26 19:52:31 +0000

    Unit test library for AirlineBookingLibrary was added (using Xunit) and StringExtensions test added.

---
+++ AirlineBookingLibrary.Tests/StringExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using AirlineBookingLibrary.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AirlineBookingLibrary.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void NullIfEmpty_ReturnsNullCorrectly()
+        {
+            // Arrange
+            object expected = null;
+
+            // Act
+            string str = "";
+            string actual = str.NullIfEmpty();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
