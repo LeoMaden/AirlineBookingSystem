ad59d29f6292fda0a9523cd2c2b22087d6a7f336
Leo Maden <leo.c.maden@gmail.com>
2019-01-15 20:13:15 +0000

    Added messages class to store reusable messages for the program.

---
+++ AirlineBookingLibrary/Helpers/Messages.cs
@@ -0,0 +1,22 @@
+﻿using AirlineBookingLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineBookingLibrary.Helpers
+{
+    public static class Messages
+    {
+        public static string GetBookingConfirmationEmailSubject(Booking booking)
+        {
+            return "";
+        }
+
+        public static string GetBookingConfirmationEmailBody(Booking booking, User user)
+        {
+            return "";
+        }
+    }
+}
