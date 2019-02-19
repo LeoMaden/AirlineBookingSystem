f34fdfc214213c1999108f8091948f8b2f61b6d1
leo <leo.c.maden@gmail.com>
2019-02-19 19:56:01 +0000

    Added search filter partial view and basic search controller and index view.

---
+++ WebUI/Controllers/SearchController.cs
@@ -0,0 +1,96 @@
+﻿using AirlineBookingLibrary.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Models;
+using WebUI.Helpers;
+using AirlineBookingLibrary.Models;
+using System.Threading.Tasks;
+using AirlineBookingLibrary.Data;
+
+namespace WebUI.Controllers
+{
+    [AllowAnonymous]
+    public class SearchController : Controller
+    {
+        private readonly IFlightManager _flightManager;
+        private readonly IDataAccess _dataAccess;
+
+        public SearchController(IDataAccess dataAccess, IFlightManager flightManager)
+        {
+            _dataAccess = dataAccess;
+            _flightManager = flightManager;
+        }
+
+        // GET: Search
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            var searchFilterModel = new SearchFilterModel();
+
+            searchFilterModel.Airports = await GetAirportSelectListAsync();
+
+            return View(searchFilterModel);
+        }
+
+        // POST: Search
+        [HttpPost]
+        public async Task<ActionResult> Index(SearchFilterModel searchFilterModel)
+        {
+            // Check model is valid.
+            if (!ModelState.IsValid)
+            {
+                // Repopulate drowdown list box.
+                searchFilterModel.Airports = await GetAirportSelectListAsync();
+
+                return View(searchFilterModel);
+            }
+
+            // Get Airport objects from database using their Ids.
+            Airport originAirport = await _dataAccess.FindAirportByIdAsync(searchFilterModel.SelectedOriginAirportId);
+            Airport destinationAirport = await _dataAccess.FindAirportByIdAsync(searchFilterModel.SelectedDestinationAirportId);
+
+
+            // Set inbound date to the value in the model if return flight,
+            // otherwise the inbound date is a default DateTime().
+            DateTime inboundDate = new DateTime();
+
+            if (searchFilterModel.ReturnFlight == true)
+            {
+                inboundDate = searchFilterModel.InboundDate.Value;
+            }
+
+
+            // Map SearchFilterModel to SearchFilterParameters.
+            SearchFilterParameters searchFilterParameters = new SearchFilterParameters
+            {
+                OriginAirport = originAirport,
+                DestinationAirport = destinationAirport,
+                OutDate = searchFilterModel.OutboundDate,
+                InDate = inboundDate
+            };
+
+            var cheapestPricesOnSimilarDays = await _flightManager.FindCheapestPricesOnSimilarDatesAsync(searchFilterParameters, 2);
+
+
+
+            return View(searchFilterModel);
+        }
+
+        private SelectList ToSelectList(List<Airport> airports)
+        {
+            SelectList output = new SelectList(airports, nameof(Airport.Id), nameof(Airport.FriendlyName));
+
+            return output;
+        }
+
+        private async Task<SelectList> GetAirportSelectListAsync()
+        {
+            List<Airport> airports = (await _dataAccess.GetAirportsAsync()).ToList();
+
+            return ToSelectList(airports);
+        }
+    }
+}
---
+++ WebUI/Models/SearchFilterModel.cs
@@ -0,0 +1,49 @@
+﻿using AirlineBookingLibrary.Enums;
+using AirlineBookingLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Models
+{
+    public class SearchFilterModel
+    {
+        public SelectList Airports { get; set; }
+
+        [Required]
+        public int SelectedOriginAirportId { get; set; }
+
+        [Required]
+        public int SelectedDestinationAirportId { get; set; }
+
+
+
+
+        [Required(ErrorMessage = "Please select a departure date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime OutboundDate { get; set; } = DateTime.Today;
+
+        [Required(ErrorMessage = "Please select one way/return flight")]
+        public bool ReturnFlight { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? InboundDate { get; set; } = DateTime.Today.AddDays(7);
+
+        [Range(1, 8, ErrorMessage = "Number of adults must be between 1 and 8")]
+        public int NumberAdults { get; set; }
+
+        [Range(0, 8, ErrorMessage = "Number of adults must be between 0 and 8")]
+        public int NumberChildren { get; set; }
+
+        [EnumDataType(typeof(TravelClass))]
+        public TravelClass TravelClass { get; set; }
+
+
+
+    }
+}
